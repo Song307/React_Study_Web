a9a7af5d18404930339a03f280a0ff43893b361f
Song307 <ssongsyj0203@gmail.com>
2024-07-18 17:23:38 +0900

    1

--- src/mainSrc/Navigator.js
+++ src/mainSrc/Navigator.js
@@ -7,8 +7,8 @@ function Navigator(){
     return(
         <div className="Navigator">
             <div className='Nav'>
-            <ul>
-            <div className='Nav_icon'>
+                <ul>
+                <div className='Nav_icon'>
                     <a href='/'>
                     <svg xmlns="D:\web\study\public\house-door-fill.svg" width="30" height="30" fill="currentColor" class="bi bi-house-door-fill" className='icon' viewBox="0 0 16 16">
                     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/></svg>
@@ -43,7 +43,7 @@ function Navigator(){
                     </svg>
                     </a></li>
                     </div>
-                    
+
                     <li><div className='input_box'>
                     <input placeholder='Search'></input>
                     <div className='Search_button'>
@@ -54,12 +54,7 @@ function Navigator(){
                 </div>
                 </li>
                 </div>
-            </ul>
-            
-            
-                
-                
-                
+                </ul>
             </div>
         </div>
     )
